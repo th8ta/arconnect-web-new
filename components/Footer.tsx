2004a951ed222e4fcc21b2e37a7119013a992c27
mojpm <itsjoelpm@gmail.com>
2021-10-28 02:47:45 +0300

    chore: footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,19 +1,34 @@
+import Link from "next/link";
 import styles from "styles/components/footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footerContainer}>
-      <ul>
-        <h4>Resources</h4>
-        <li>Documentation</li>
-        <li>FAQ</li>
-        <li>Blog</li>
-        <li>Arweave</li>
-      </ul>
-      <ul>
-        <h4>Legal</h4>
-        <li>License</li>
-      </ul>
+      <div>
+        <ul>
+          <h4>Resources</h4>
+          <li>
+            <Link href="/docs">Documentation</Link>
+          </li>
+          <li>
+            <Link href="/">FAQ</Link>
+          </li>
+          <li>
+            <Link href="https://blog.th8ta.org/">Blog</Link>
+          </li>
+          <li>
+            <Link href="https://arweave.org/">Arweave</Link>
+          </li>
+        </ul>
+        <ul>
+          <h4>Legal</h4>
+          <li>
+            <Link href="https://github.com/th8ta/ArConnect/blob/development/LICENSE">
+              License
+            </Link>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
