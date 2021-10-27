a31b72d49d00eef2349bf11d5e70713d4b6602c5
mojpm <itsjoelpm@gmail.com>
2021-10-27 05:39:36 +0300

    implementation: navbar animation

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { detect } from "detect-browser";
 import { useEffect, useState } from "react";
-
+import { GrClose } from "react-icons/gr";
 import styles from "styles/components/nav.module.scss";
 
 export default function Nav() {
@@ -17,58 +17,67 @@ export default function Nav() {
   }, []);
 
   return (
-    <nav className={styles.Nav}>
-      <div className={openNav ? styles.hide : styles.logo}>
-        <Image
-          src="/assets/logo.svg"
-          alt="arconnect logo"
-          width={30}
-          height={30}
-        />
-        <p>ArConnect</p>
-      </div>
-      <ul
-        className={
-          openNav ? `${styles.navItems} ${styles.showNav}` : styles.navItems
-        }
-      >
-        <div onClick={() => setOpenNav(false)}>
+    <>
+      <nav className={styles.Nav}>
+        <div className={styles.logo}>
           <Image
-            src="/assets/closeNavbar.svg"
+            src="/assets/logo.svg"
             alt="arconnect logo"
             width={30}
             height={30}
           />
+          <p>ArConnect</p>
         </div>
-        <li>
-          <Link href="#">Home</Link>
-        </li>
-        <li>
-          <Link href="#">Developers</Link>
-        </li>
-        <li className={styles.security}>
-          <Link href="#">Security</Link>
-        </li>
-        <li>
+
+        <ul className={styles.navItems}>
+          <li>
+            <Link href="#">Home</Link>
+          </li>
+          <li>
+            <Link href="#">Developers</Link>
+          </li>
+          <li className={styles.security}>
+            <Link href="#">Security</Link>
+          </li>
+        </ul>
+
+        {openNav ? (
+          <div onClick={() => setOpenNav(false)} className={styles.closeMenu}>
+            <GrClose size="1.8rem" />
+          </div>
+        ) : (
+          <div className={styles.burgerMenu} onClick={() => setOpenNav(true)}>
+            <Image
+              src="/assets/navBurgerMenu.svg"
+              alt="arconnect logo"
+              width={30}
+              height={30}
+            />
+          </div>
+        )}
+
+        <div className={styles.addToBrowsers}>
           <Link href="#">
             {browser === "firefox" || browser === "ff"
               ? "Add to Firefox"
               : `Add to Chrome`}
           </Link>
-        </li>
-      </ul>
+        </div>
+      </nav>
 
-      <div
-        className={openNav ? styles.hide : styles.burgerMenu}
-        onClick={() => setOpenNav(true)}
-      >
-        <Image
-          src="/assets/navBurgerMenu.svg"
-          alt="arconnect logo"
-          width={30}
-          height={30}
-        />
+      <div className={styles.mobileNav}>
+        <ul className={openNav ? styles.showNav : styles.closeNav}>
+          <li>
+            <Link href="#">Home</Link>
+          </li>
+          <li>
+            <Link href="#">Developers</Link>
+          </li>
+          <li className={styles.security}>
+            <Link href="#">Security</Link>
+          </li>
+        </ul>
       </div>
-    </nav>
+    </>
   );
 }
