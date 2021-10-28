ed761c4ab112d6ff349c761c374f5ca6fa6df6a3
mojpm <itsjoelpm@gmail.com>
2021-10-29 02:57:05 +0300

    chore: Community component

---
+++ components/Home/Community/index.tsx
@@ -0,0 +1,66 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+const Community = () => {
+  const Card = ({
+    platform,
+    platformIcon,
+    text,
+    linkTo
+  }: {
+    platform: string;
+    platformIcon: string;
+    text: string;
+    linkTo: string;
+  }) => (
+    <Link href={`${linkTo}`}>
+      <a target="_blank" className={styles.cardWrapper}>
+        <div>
+          <Image src={platformIcon} alt={platform} width={25} height={25} />
+          <p className={styles.platform}>{platform}</p>
+        </div>
+        <p>{text}</p> 
+      </a>
+    </Link>
+  );
+
+  return (
+    <section className={styles.container}>
+      <div className={styles.introSection}>
+        <h2>Community Driven</h2>
+        <p>
+          Join ArConnect&apos;s decentralized community and interact with the
+          world on Discord or Twitter.
+        </p>
+
+        <Link href="https://verto.exchange/chat">
+          <a className={styles.chatBtn}>Come join us</a>
+        </Link>
+      </div>
+
+      <section>
+        <Card
+          linkTo="https://www.verto.exchange/chat"
+          platform="Discord"
+          platformIcon="/assets/discord.svg"
+          text="With ArConnect's user-friendly interface, you can manage your Arweave balance, view and transfer assets and see your latest "
+        />
+        <Card
+          linkTo="https://twitter.com/vertoexchange"
+          platform="Twitter"
+          platformIcon="/assets/twitter.svg"
+          text="With ArConnect's user-friendly interface, you can manage your Arweave balance, view and transfer assets and see your latest "
+        />
+        <Card
+          linkTo="https://github.com/th8ta/arconnect"
+          platform="Github"
+          platformIcon="/assets/github.svg"
+          text="With ArConnect's user-friendly interface, you can manage your Arweave balance, view and transfer assets and see your latest "
+        />
+      </section>
+    </section>
+  );
+};
+
+export default Community;
