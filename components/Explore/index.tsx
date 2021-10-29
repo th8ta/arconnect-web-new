58921b06db5e73bc6b9894c884346ce15a98fda8
mojpm <itsjoelpm@gmail.com>
2021-10-30 02:58:02 +0300

    fix: markup

--- components/Explore/index.tsx
+++ components/Explore/index.tsx
@@ -1,18 +1,55 @@
 import Link from "next/link";
 import Image from "next/image";
-import styles from "styles/components/Explore/explore.module.scss";
 
-const JustExplore = () => {
+import ExplorePage from "./ExplorePage";
+import styles from "./styles.module.scss";
+
+export const StoreAR = () => {
+  return (
+    <section className={styles.dappsContainer}>
+      <ExplorePage
+        iconUrl="/assets/moneyBag.svg"
+        altText="money bag emoji"
+        heading="Store you first AR tokens"
+        introParagraph="Please click the <span>Download</span> button on this page to get ArConnect for your
+        browser if you haven’t already, If you have then let’s proceed! You
+        basically just have to complete the following actions below step by step"
+      />
+    </section>
+  );
+};
+
+export const AccessDapps = () => {
+  return (
+    <section className={styles.dappsContainer}>
+      <ExplorePage
+        iconUrl="/assets/plug.svg"
+        altText="plug emoji"
+        accessDapps
+        heading="Access a Dapp"
+        introParagraph="Please click the <span>Download</span> button on this page to get ArConnect for your browser if you haven’t already, If you have then let’s proceed!
+      <br></br>
+      
+        ArConnect is the easiest way to access Dapps on Arweave, but you’ll need to have some AR tokens to complete most transactions
+        <br></br>
+        
+        
+        Steps <strong>(1.)</strong> - <strong>(5.)</strong> walk you through setting up your wallet to receive AR, please skip if you already have AR in your wallet"
+      />
+    </section>
+  );
+};
+
+export const JustExplore = () => {
   return (
     <main className={styles.mainContainer}>
       <div className={styles.headerContainer}>
-        <h1>I just want to explore</h1>
-        <div className={styles.nerdFace}>
-          <Image
+        <h1>I just want to explore </h1>
+        <div>
+          <img
             src="/assets/nerdFace.svg"
-            height={42}
-            width={42}
             alt="ArConnect Logo"
+            className={styles.nerdFace}
           />
         </div>
       </div>
@@ -21,7 +58,7 @@ const JustExplore = () => {
         you have never used ArConnect before, we really recommend that you check
         out <Link href="#">how to store your first AR token</Link>, either way,
         you can always find learning resources on the
-        <Link href="#">ArConnect 101 page</Link>
+        <Link href="#"> ArConnect 101 page</Link>
       </p>
       <div className={styles.arConnect}>
         <Image
@@ -31,9 +68,10 @@ const JustExplore = () => {
           alt="ArConnect Logo"
         />
       </div>
-      <button>Add to Brave</button>
+
+      <Link href="/">
+        <a className={styles.browserBtn}> Add to Brave</a>
+      </Link>
     </main>
   );
 };
-
-export default JustExplore;
