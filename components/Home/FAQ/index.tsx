6c1dc084719261dfe8c6edbadc670a32410b4eeb
mojpm <itsjoelpm@gmail.com>
2021-11-08 16:46:16 +0300

    chore: move QandA component

--- components/Home/FAQ/index.tsx
+++ components/Home/FAQ/index.tsx
@@ -1,44 +1,6 @@
-import Link from 'next/link';
 import Image from 'next/image';
-import { useState } from 'react';
+import QandA from 'components/QandA';
 import styles from './styles.module.scss';
-
-const QandA = ({
-  question,
-  answer,
-  special
-}: {
-  question: string;
-  answer: string;
-  special?: boolean;
-}) => {
-  const [isOpen, setIsOpen] = useState(false);
-
-  return (
-    <div
-      className={styles.questionAnswer}
-      onClick={() => setIsOpen(!isOpen)}
-      id={special ? styles.special : ''}
-    >
-      <div className={styles.question}>
-        <div
-          className={!isOpen ? styles.qn : `${styles.qn} ${styles.qnActive}`}
-        >
-          {question}
-        </div>
-        <div className={styles.icon}>
-          <Image
-            src={isOpen ? '/assets/upArrow.svg' : '/assets/downArrow.svg'}
-            alt="dropdown icon"
-            width={14}
-            height={14}
-          />
-        </div>
-      </div>
-      <p className={isOpen ? styles.answer : styles.answerActive}>{answer}</p>
-    </div>
-  );
-};
 
 const FeedbackCard = ({
   name,
@@ -75,41 +37,7 @@ const FeedbackCard = ({
 const FAQ = () => {
   return (
     <section className={styles.container}>
-      <section className={styles.faq}>
-        <div>
-          <h2>Here to answer your questions</h2>
-          <p>
-            ArConnect is a completely decentralized network built on top of the
-            blockweave. Anyone can help power the exchange. Feel freed to reach
-            out to the community on Discord or Twitter!
-          </p>
-          <Link href="https://verto.exchange/chat">
-            <a className={styles.chatBtn}>Chat with us</a>
-          </Link>
-        </div>
-
-        <section>
-          <QandA
-            special
-            question="What is Arconnect?"
-            answer="ArConnect is the most user friendly crypto wallet you will ever use. If you're here, it means you're super early, please enter your email to be one of the first users."
-          />
-          <QandA
-            question="What is a Non-custodial wallet?"
-            answer="Coming Soon"
-          />
-          <QandA question="What is a Browser extension?" answer="Coming Soon" />
-          <QandA
-            question="What happens to my old Arweave wallet?"
-            answer="Coming Soon"
-          />
-          <QandA
-            question="How does ArConnect protect my assets?"
-            answer="Coming Soon"
-          />
-        </section>
-      </section>
-
+      <QandA />
       <section className={styles.feedbackContainer}>
         <h2>
           10,000+ people <span>and counting</span> love ArConnect
