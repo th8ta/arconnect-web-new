1ebb94aa7ed60e204a2b2a7f1a72003b1e1d4e9e
Tate Berenbaum <tateflyer1@gmail.com>
2022-03-20 22:47:41 -0400

    chore: update head for SEO

--- components/Metas.tsx
+++ components/Metas.tsx
@@ -12,6 +12,7 @@ const Metas = ({ title, description, image, largeImage }: Props) => {
     <Head>
       {title && (
         <>
+          <title>{title}</title>
           <meta property="og:title" content={title} />
           <meta name="twitter:title" content={title} />
         </>
@@ -25,6 +26,7 @@ const Metas = ({ title, description, image, largeImage }: Props) => {
       )}
       {image && (
         <>
+          <link rel="shortcut icon" href={image} />
           <meta property="og:image" content={image} />
           <meta name="twitter:image" content={image} />
           {largeImage && (
